c6b0aca66319692ccf6ac6f60c18210508c89375
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2025-08-12 12:39:11 +0200

    try it now

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { Toaster } from "react-hot-toast";
 export const metadata: Metadata = {
   title: "Yale DegreeIntelligence",
   description:
-    "Your Yale degree, made easy. Stress-free. Always. Built by yalies, for yalies.",
+    "Organize your Yale academic life. Stress-free. Always. Built by yalies, for yalies.",
   icons: [{ rel: "icon", url: "/favicon.ico" }],
   openGraph: {
     title: "Yale DegreeIntelligence",
